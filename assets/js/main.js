f5b32be8893fe945d9a332b803ba8a0c25cc8eff
rega <jihandokoengg@gmail.com>
2021-01-09 16:39:19 +0700

    Edit grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,11 +59,11 @@ sr.reveal('.about__img', { delay: 500 })
 sr.reveal('.about__subtitle', { delay: 300 })
 sr.reveal('.about__profession', { delay: 400 })
 sr.reveal('.about__text', { delay: 500 })
-sr.reveal('.about__social-icon', { delay: 600/* , interval: 200 */ })
+sr.reveal('.about__social-icon', { delay: 600, interval: 200 })
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', { distance: '20px', delay: 50/* , interval: 100 */ })
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
 sr.reveal('.skills__img', { delay: 400 })
 
 /*SCROLL PORTFOLIO*/
@@ -71,7 +71,7 @@ sr.reveal('.portfolio__img', { interval: 300 })
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {/* interval: 200 */ })
+sr.reveal('.contact__text', { interval: 200 })
 sr.reveal('.contact__input', { delay: 400 })
 sr.reveal('.contact__button', { delay: 600 })
 
