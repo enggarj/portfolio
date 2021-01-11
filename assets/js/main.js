b07af4f86344a1ea484940c14b8ef955a2b9f979
rega <jihandokoengg@gmail.com>
2021-01-11 11:56:05 +0700

    Edit icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId)
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
+	if (toggle && nav) {
+		toggle.addEventListener('click', () => {
+			nav.classList.toggle('show')
+		})
+	}
 }
 showMenu('nav-toggle', 'nav-menu')
 
@@ -15,8 +15,8 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show')
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -26,27 +26,27 @@ const sections = document.querySelectorAll('section[id]')
 window.addEventListener('scroll', scrollActive)
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
+	const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+		} else {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+		}
+	})
 }
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+	origin: 'top',
+	distance: '80px',
+	duration: 2000,
+	reset: true
 })
 
 /*SCROLL HOME*/
